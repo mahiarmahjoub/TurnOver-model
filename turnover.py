8d60288f40ebab34862ebf529670db78db07f01a
mahiarmahjoub <31858737+mahiarmahjoub@users.noreply.github.com>
2017-09-14 16:53:02 +0100

    Update turnover.py
    Included: 
    - file import and processing 
    - plotting 
    Still to do: 
    - fix the optimisation code

--- turnover.py
+++ turnover.py
@@ -2,29 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import pandas as pd 
-from pandas import Series,DataFrame 
+from scipy import optimize
+from scipy.special import lambertw
 
-time = np.arange(0,100,0.5)
-#ntime = len(time)
-#fluo = 10*time - np.random.exponential(5,ntime)
-#plt.plot(fluo)
 
 ## Import & process fluorescence data 
+filename = '160816_M24_pH8_80ugmL.csv'
+fluo_data = pd.read_csv(filename)   # import 
+[xname,yname] = fluo_data.columns   # extract column names 
+expfluo = fluo_data[yname][:]       # create vector containing fluo values
+maxindex = np.argmax(expfluo)       # index of max fluorescence value 
+expfluo_norm = expfluo[:maxindex] - expfluo[0]  # normalise
+expfluo_normrel = expfluo_norm/np.amax(expfluo_norm)    # relative data
+time = fluo_data[xname][:maxindex]  # extract time vector 
 
 
 ## Parameters 
-kd = 10
-ki = 5
-mtot = 9.6e-6
-Atot = 1e-1
+#kd = 10
+#ki = 5
+#kj = 10
+mtot = 9.6e-6   # total concentration of monomers 
+Atot = 1e-1 # total area of available interface
+#p = np.array([kd, kj, ki, mtot, Atot])
 
 
 ## Solution for M(t) when Acov steady state 
-#def Mss_sol(t,M):
-#    return 
-
-
+#def Mss_sol(p, t):
+#    y11 = p[2]*mtot/(p[0]+p[1])
+#    y12 = p[2]*(mtot-(Atot*p[0]*t))/(p[0]+p[1])
+#    y1 = y11*np.exp(y12)
+#    return mtot - ((p[0]+p[1])/p[2])*lambertw(y1)
+    
+Mss_sol = lambda p, t: mtot - ((p[0]+p[1])/p[2])*lambertw(p[2]*mtot/(p[0]+p[1])*np.exp(p[2]*(mtot-(Atot*p[0]*t))/(p[0]+p[1])))
 ## Optimise fluorescence data to the steady state solution 
+errfunc = lambda p, t, y: Mss_sol(p, t) - y # Distance to the target function
+p0 = [1, 1, 1]     # Initial guess for the parameters
+pfitted = optimize.leastsq(errfunc, p0[:], args=(time,expfluo_normrel))    # fit 
+
+[kd, kj, ki] = pfitted
 
 
 ## ODE Numerical evaluator 
@@ -33,12 +48,16 @@ def dx_dt(x,t):
 
 IVs = [0,0]
 X = odeint(dx_dt, IVs, time)
-Acov = X[:,0]
-M = X[:,1]
+Acov_num = X[:,0]
+Mnum = X[:,1]
+Mnum_rel = Mnum/mtot
 
 
 ## Plot all data i.e. experimental, optimised and ODE evaluation 
-plt.plot(time,M)
+plt.plot(time,expfluo_normrel, label='Experimental')
+plt.plot(time,Mnum_rel,'r', label='Numerical')
+plt.plot(time, Mss_sol(time,param)/mtot,'b', label='Steady-state')
 plt.xlabel("Time")
 plt.ylabel("Aggregate conc.")
-plt.title("Turn Over model");
+plt.title("Turn Over model")
+plt.legend()
