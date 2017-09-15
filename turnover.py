4a6bf3d1ad75527ee6ca2dc9b0e5c10ecd7d5702
mahiarmahjoub <mahiarmahjoub@gmail.com>
2017-09-15 16:25:58 +0100

    Rid optimisation of error due to complex output from lambertw AND constrained parameters to absolute values

--- turnover.py
+++ turnover.py
@@ -18,36 +18,40 @@ time = fluo_data[xname][:maxindex]  # extract time vector
 
 
 ## Parameters 
-#kd = 10
-#ki = 5
-#kj = 10
+kd = 10
+ki = 5
+kj = 10
 mtot = 9.6e-6   # total concentration of monomers 
 Atot = 1e-1 # total area of available interface
 #p = np.array([kd, kj, ki, mtot, Atot])
 
 
 ## Solution for M(t) when Acov steady state 
-#def Mss_sol(p, t):
-#    y11 = p[2]*mtot/(p[0]+p[1])
-#    y12 = p[2]*(mtot-(Atot*p[0]*t))/(p[0]+p[1])
-#    y1 = y11*np.exp(y12)
-#    return mtot - ((p[0]+p[1])/p[2])*lambertw(y1)
+def Mss_sol(p, t):
+    a = abs(p[0])
+    b = abs(p[1])
+    c = abs(p[2])
+    y11 = c*mtot/(a+b)
+    y12 = c*(mtot-(Atot*a*t))/(a+b)
+    y1 = y11*np.exp(y12)
+    out = mtot - ((a+b)/c)*np.real(lambertw(y1))
+    return out
     
-Mss_sol = lambda p, t: mtot - ((p[0]+p[1])/p[2])*lambertw(p[2]*mtot/(p[0]+p[1])*np.exp(p[2]*(mtot-(Atot*p[0]*t))/(p[0]+p[1])))
+#Mss_sol = lambda p, t: mtot - ((p[0]+p[1])/p[2])*lambertw(p[2]*mtot/(p[0]+p[1])*np.exp(p[2]*(mtot-(Atot*p[0]*t))/(p[0]+p[1])))
 ## Optimise fluorescence data to the steady state solution 
 errfunc = lambda p, t, y: Mss_sol(p, t) - y # Distance to the target function
-p0 = [1, 1, 1]     # Initial guess for the parameters
-pfitted = optimize.leastsq(errfunc, p0[:], args=(time,expfluo_normrel))    # fit 
+p0 = [kd,ki,kj]     # Initial guess for the parameters
+pfitted,success = optimize.leastsq(errfunc, p0[:], args=(time,expfluo_normrel))  # fit 
 
-[kd, kj, ki] = pfitted
+[kd, ki, kj] = abs(pfitted)
 
 
 ## ODE Numerical evaluator 
-def dx_dt(x,t):
+def dx_dt(x,t,kd,ki,kj):
     return [-kd*x[0] + ki*(mtot-x[1])*(Atot-x[0]), kd*x[0]]
 
 IVs = [0,0]
-X = odeint(dx_dt, IVs, time)
+X = odeint(dx_dt, IVs, time, args=(kd,ki,kj))
 Acov_num = X[:,0]
 Mnum = X[:,1]
 Mnum_rel = Mnum/mtot
@@ -56,7 +60,7 @@ Mnum_rel = Mnum/mtot
 ## Plot all data i.e. experimental, optimised and ODE evaluation 
 plt.plot(time,expfluo_normrel, label='Experimental')
 plt.plot(time,Mnum_rel,'r', label='Numerical')
-plt.plot(time, Mss_sol(time,param)/mtot,'b', label='Steady-state')
+plt.plot(time, Mss_sol(pfitted,time)/mtot,'b', label='Steady-state')
 plt.xlabel("Time")
 plt.ylabel("Aggregate conc.")
 plt.title("Turn Over model")
