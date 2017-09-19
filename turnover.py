89f71d0ac734f9c4976b1c4a378e44aa79245d8d
mahiarmahjoub <mahiarmahjoub@gmail.com>
2017-09-19 17:44:12 +0100

    File import, processing works well. ODE numerical evaluator and the analytical solutions agree. Plotting works. ISSUES: optimisation needs further work ...

--- turnover.py
+++ turnover.py
@@ -1,3 +1,12 @@
+## Main program
+
+# function of this program:
+    # import and process i.e. normalise etc. fluorescence data
+    # fit steady state solution to the experimental data 
+    # use fitted parameters for the ODE numerical evaluation and cross-checking 
+
+
+## Required packages 
 import numpy as np 
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
@@ -6,9 +15,7 @@ from scipy import optimize
 from scipy.special import lambertw
 import argparse
 
-
-
-## user parameters; currently hardcoded, some might be replaced by argparse later
+## User parameters
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="insert the name of file")
 parser.add_argument("baseline", type=int,
@@ -22,54 +29,66 @@ filename = args.filename
 baseline_N_points_to_average = args.baseline
 plateau_N_points_to_average = args.endpoint
 
+#filename = 'M2480averagepH8.csv'
+#baseline_N_points_to_average = 2
+#plateau_N_points_to_average = 5
+
 
 ## Import & process fluorescence data 
 
-fluo_data = pd.read_csv(filename)   # import 
-[xname,yname] = fluo_data.columns   # extract column names 
-expfluo = fluo_data[yname][:]       # create vector containing fluo values
-#maxindex = np.argmax(expfluo)       # index of max fluorescence value 
+fluo_data = pd.read_csv(filename)    # import 
+[xname,yname] = fluo_data.columns    # extract column names 
+expfluo = fluo_data[yname]           # create vector containing fluo values
 baseline_average = np.mean(expfluo[0:baseline_N_points_to_average]) 
-plateau_average = np.mean(expfluo[plateau_N_points_to_average:-1]) 
+plateau_average = np.mean(expfluo[-plateau_N_points_to_average:-1]) 
 expfluo_norm = expfluo - baseline_average  # normalise
 expfluo_normrel = expfluo_norm/(plateau_average - baseline_average)  # relative data
 time = fluo_data[xname]  # extract time vector 
 
 ## Parameters 
-kd = 10
-ki = 5
-kj = 10
+kd_guess = 10 # initial guess for the optimisation
+ki_guess = 1  # initial guess for the optimisation
+kj_guess = 0.1 # initial guess for the optimisation
 mtot = 9.6e-6   # total concentration of monomers 
 Atot = 1e-1 # total area of available interface
-#p = np.array([kd, kj, ki, mtot, Atot])
+Acov_initial = 0 
+M_initial = 0
 
 
 ## Solution for M(t) when Acov steady state 
-def Mss_sol(p, t):
+def Mss_sol(p,t,mTot,ATot,Mini): 
     a = abs(p[0])
     b = abs(p[1])
     c = abs(p[2])
-    y11 = c*mtot/(a+b)
-    y12 = c*(mtot-(Atot*a*t))/(a+b)
-    y1 = y11*np.exp(y12)
-    out = mtot - ((a+b)/c)*np.real(lambertw(y1))
+    out1 = (a + c)/b
+    out2 = (b*(mTot-Mini))/(a + c)
+    out3 = (b*ATot*a*t)/(a + c)
+    out4 = np.exp(out2 - out3)
+    out5 = np.real(lambertw(out2*out4))
+    
+    out = mTot - out5*out1
     return out
     
-#Mss_sol = lambda p, t: mtot - ((p[0]+p[1])/p[2])*lambertw(p[2]*mtot/(p[0]+p[1])*np.exp(p[2]*(mtot-(Atot*p[0]*t))/(p[0]+p[1])))
 ## Optimise fluorescence data to the steady state solution 
-errfunc = lambda p, t, y: Mss_sol(p, t) - y # Distance to the target function
-p0 = [kd,ki,kj]     # Initial guess for the parameters
-pfitted,success = optimize.leastsq(errfunc, p0[:], args=(time,expfluo_normrel))  # fit 
+errfunc = lambda p,t,y,mTot,ATot,Mini: Mss_sol(p,t,mTot,ATot,Mini) - y # Distance to the target function
 
+
+p0 = [kd_guess,ki_guess,kj_guess]     # Initial guess for the parameters
+fit_result = optimize.leastsq(errfunc,
+                        p0[:], 
+                         args=(time,expfluo_normrel,mtot,Atot,M_initial),
+                         full_output = True,
+                         ftol = 1e-12, maxfev = 1000) # fit 
+
+pfitted = fit_result[0]
 [kd, ki, kj] = abs(pfitted)
 
-
 ## ODE Numerical evaluator 
-def dx_dt(x,t,kd,ki,kj):
-    return [-kd*x[0] + ki*(mtot-x[1])*(Atot-x[0]), kd*x[0]]
+def dx_dt(x,t,kd,ki,kj,mTot,ATot):
+    return [-(kd+kj)*x[0] + ki*(mTot-x[1])*(ATot-x[0]), kd*x[0]]
 
-IVs = [0,0]
-X = odeint(dx_dt, IVs, time, args=(kd,ki,kj))
+IVs = [Acov_initial, M_initial]
+X = odeint(dx_dt, IVs, time, args=(kd, ki, kj, mtot, Atot))
 Acov_num = X[:,0]
 Mnum = X[:,1]
 Mnum_rel = Mnum/mtot
@@ -78,7 +97,8 @@ Mnum_rel = Mnum/mtot
 ## Plot all data i.e. experimental, optimised and ODE evaluation 
 plt.plot(time,expfluo_normrel, label='Experimental')
 plt.plot(time,Mnum_rel,'r', label='Numerical')
-plt.plot(time, Mss_sol(pfitted,time)/mtot,'b', label='Steady-state')
+plt.plot(time, Mss_sol(abs(pfitted),time,mtot,Atot,M_initial)/mtot, 'b', label='Steady-state')
+#plt.plot(time, Mss_sol(p0,time,mtot,Atot,M_initial)/mtot, 'b', label='Steady-state')
 plt.xlabel("Time")
 plt.ylabel("Aggregate conc.")
 plt.title("Turn Over model")
